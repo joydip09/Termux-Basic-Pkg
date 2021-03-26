55f318d56fe1a66938c1f6ae107e892d8418ac52
Hydra83 <75995283+Hydra83@users.noreply.github.com>
2021-03-26 23:30:09 +0600

    Update update.sh

--- update.sh
+++ update.sh
@@ -1,6 +1,4 @@
 cd
 rm -rf Termux-Basic-Packages
 git clone https://github.com/Hydra83/Termux-Basic-Packages
-cd
-cd Termux-Basic-Packages
 echo "Your Termux-Basic-Packages Tool Has Updated" | lolcat -as 10 
