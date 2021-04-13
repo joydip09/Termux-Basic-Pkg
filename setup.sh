e0dcc162583925fef5dd3250dec8173f59c8a796
Hydra83 <75995283+Hydra83@users.noreply.github.com>
2021-04-13 23:02:45 +0600

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -4,4 +4,4 @@ mv Termux $PREFIX/bin/
 chmod +x $PREFIX/bin/Termux
 cd
 Termux
-echo "If you want to use my tool next time just write "$Termux" and ENTER" | lolcat -as 10
+echo "If you want to use my tool next time just write $Termux and ENTER" | lolcat -as 10
