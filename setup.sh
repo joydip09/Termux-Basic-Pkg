97e873fe8951b49edb5b1dd2f4677ba703304e02
C_Joydip <75995283+C-Joydip@users.noreply.github.com>
2021-07-06 16:04:11 +0600

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,9 +1,15 @@
 #!bin/bash
 
+apt update -y
+apt upgrade -y
+apt install ruby -y
+gem install lolcat
+echo "Press Allow in next pop-up" | lolcat -as 10
+termux-setup-storage
 clear
 rm -rf $PREFIX/bin/Termux
 mv Termux $PREFIX/bin/
 chmod +x $PREFIX/bin/Termux
 cd
 Termux
-echo "If you want to use my tool next time just write $Termux and ENTER" | lolcat -as 10
+echo "If you want to use my tool next time just write $Termux and ENTER" | lolcat -as 5
