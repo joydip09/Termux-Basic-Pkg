f396136da7f3088e905a3f19f38cda969e91340f
C_Joydip <75995283+C-Joydip@users.noreply.github.com>
2021-07-06 16:02:55 +0600

    Update update.sh

--- update.sh
+++ update.sh
@@ -1,9 +1,15 @@
 #!bin/bash
 
+apt update -y
+apt upgrade -y
+apt install ruby -y
+gem install lolcat
+echo "Press Allow in next pop-up" | lolcat -as 10
+termux-setup-storage
 cd $PREFIX/bin/
 rm -rf Termux
 cd
 rm -rf Termux-Basic-Packages
-git clone https://github.com/Hydra83/Termux-Basic-Packages
+git clone https://github.com/C-Joydip/Termux-Basic-Packages
 cd
 echo "Your Termux-Basic-Packages Tool Has Updated" | lolcat -as 10 
